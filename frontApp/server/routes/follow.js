512260fb4be66ae594772760eb7aaa188fbe0cc0
Jiwon <jeewonre@naver.com>
2019-04-06 20:40:04 +0900

    2019.04.06
    - 팔로우에서 사용자 팔로우, 팔로잉 유저 아이콘 보기

--- frontApp/server/routes/follow.js
+++ frontApp/server/routes/follow.js
@@ -8,17 +8,26 @@ const { User,Page,Hashtag } = require('../../models');
 var router = express.Router();
 
 router.get('/',isLoggedIn, async(req,res,next) => {
-    console.log(req.user);
+    console.log(req.user.Followers);
     const user = await User.find({ where : { id : req.user.id } });
-    console.log(user.username);
+    let followers = [];
+    let followings = [];
     let followTag = [];
     if (user) {
     followTag = await user.getHashtags({ include : [{ model : Page, include: [User,Hashtag] }] });
+    for(var i=0;i < req.user.Followers.length;i++) {
+        followers = await User.findAll({ where : {id : req.user.Followers[i].id } });
+        }
+    for(var j=0;j < req.user.Followings.length;j++) {
+        followings = await User.findAll({ where : {id : req.user.Followings[j].id } });
+        }
     }
     res.render('follow', {
         title:`follow | Anamorph`,
         user : req.user,
         followTag : followTag,
+        followers : followers,
+        followings : followings,
         loginError : req.flash('loginError'),
     });
 });
