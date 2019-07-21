0a1c0a5b9a59cbfdb8a108e32a8d625c53689087
Jiwon <jeewonre@naver.com>
2019-07-21 14:54:15 +0900

    Develop 분기 생성

--- frontApp/server/routes/cloudStorage.js
+++ frontApp/server/routes/cloudStorage.js
@@ -2,15 +2,20 @@ const express = require('express');
 const gravatar = require('gravatar');
 
 const { Page,Hashtag,User,Comment,Like } = require('../../models');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.get('/', async(req, res, next) => {
+router.get('/',isLoggedIn, async(req, res, next) => {
     return res.render('cloudStorage', {
         title : `${req.user.username}'s cloud Storage | Anamorph`,
         user : req.user,
         gravatar: gravatar.url(req.user.email,{s:'80',r:'x',d:'retro'},true),
     });
+});
+router.post('/postReflex', async(req, res, next) => {
+    modelTitle = req.body.modelTitle;
+    console.log(modelTitle);
 });
 
 module.exports = router;
